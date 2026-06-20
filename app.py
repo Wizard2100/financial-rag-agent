f7ce7c6243a107c6ff6e179cc75ee64ff717e372
Wizard2100 <arjitagrawal1994@gmail.com>
2026-06-20 16:41:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,50 @@
 import streamlit as st
+import faiss
+import pickle
+import numpy as np
+
+from sentence_transformers import SentenceTransformer
 from google import genai
 
 client = genai.Client(
     api_key=st.secrets["GEMINI_API_KEY"]
 )
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="What is AI?"
-)
+model = SentenceTransformer("all-MiniLM-L6-v2")
 
-st.write(response.text)
+index = faiss.read_index("financialIndex.faiss")
+
+with open("companyChunks.pkl", "rb") as f:
+    chunks = pickle.load(f)
+
+query = st.text_input("Ask a financial question")
+
+if query:
+
+    query_vector = model.encode([query])
+
+    D, I = index.search(
+        np.array(query_vector).astype("float32"),
+        3
+    )
+
+    context = "\n".join(
+        [chunks[i] for i in I[0]]
+    )
+
+    prompt = f"""
+Context:
+{context}
+
+Question:
+{query}
+
+Answer using only the context.
+"""
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt
+    )
+
+    st.write(response.text)
